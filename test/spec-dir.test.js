84f03bac7086c44a89849f4a932c97115e33d2af
inyourtime <inyourtimeguy@gmail.com>
2024-12-08 18:58:51 +0700

    test: use test before & after for reduce test time

--- test/spec-dir.test.js
+++ test/spec-dir.test.js
@@ -2,112 +2,94 @@
 
 const path = require('node:path')
 const { test } = require('node:test')
+const assert = require('node:assert')
 
 const Fastify = require('fastify')
 const { checkSpecDir } = require('../lib/spec-dir')
 
-test('no specDir', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
+let fastify
 
+test.before(() => {
+  fastify = Fastify()
+})
+
+test.after(() => fastify.close())
+
+test('no specDir', () => {
   try {
     checkSpecDir(fastify, undefined)
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option is required')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option is required')
   }
 })
 
-test('specDir is not a string', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir is not a string', () => {
   try {
     checkSpecDir(fastify, 1)
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option must be a string or an array of strings')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option must be a string or an array of strings')
   }
 })
 
-test('specDir is not an absolute path', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir is not an absolute path', () => {
   try {
     checkSpecDir(fastify, 'foo')
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option must be an absolute path')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option must be an absolute path')
   }
 })
 
-test('specDir does not exist', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir does not exist', () => {
   try {
     checkSpecDir(fastify, '/foo')
   } catch (e) {
-    t.assert.ifError(e)
+    assert.ifError(e)
   }
 })
 
-test('specDir is not a directory', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir is not a directory', () => {
   try {
     checkSpecDir(fastify, path.join(__dirname, 'tmp', 'test-file.txt'))
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option must point to a directory')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option must point to a directory')
   }
 })
 
-test('specDir is a array', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir is a array', () => {
   try {
     checkSpecDir(fastify, [path.join(__dirname, 'openapi'), path.join(__dirname, 'tmp')])
   } catch (e) {
-    t.assert.ifError(e)
+    assert.ifError(e)
   }
 })
 
-test('specDir is a empty array', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir is a empty array', () => {
   try {
     checkSpecDir(fastify, [])
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option array requires one or more paths')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option array requires one or more paths')
   }
 })
 
-test('specDir contains duplicate paths', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir contains duplicate paths', () => {
   try {
     checkSpecDir(fastify, [path.join(__dirname, 'openapi'), path.join(__dirname, 'openapi')])
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option array contains one or more duplicate paths')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option array contains one or more duplicate paths')
   }
 })
 
-test('specDir array is containing non strings path', async (t) => {
-  const fastify = Fastify()
-  t.after(() => fastify.close())
-
+test('specDir array is containing non strings path', () => {
   try {
     checkSpecDir(fastify, [1])
   } catch (e) {
-    t.assert.ok(e)
-    t.assert.strictEqual(e.message, '"specDir" option must be a string')
+    assert.ok(e)
+    assert.strictEqual(e.message, '"specDir" option must be a string')
   }
 })
